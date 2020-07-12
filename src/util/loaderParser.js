baa4f448cda0b4073165ae47f7982df98a9c0f1e
Adam Zawierucha <zawiedev@gmail.com>
2020-07-12 14:36:22 -0700

    initial loaderparser

--- src/util/loaderParser.js
+++ src/util/loaderParser.js
@@ -1,5 +1,10 @@
 // Adam Zawierucha
 function getStudentRankings() {
+  /*
+  Inputs: None
+  Outputs: Returns Dictioanry where keys are externships, and values are
+           lists where index=rank and value=netid
+  */
   var dict = new Object()
   return dict
 }
