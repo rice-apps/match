1eff53e010e7fac6b9d3a3ac7c125f0f85be3db4
Adam Zawierucha <zawiedev@gmail.com>
2020-07-12 17:29:02 -0700

    student class

--- src/util/objects.js
+++ src/util/objects.js
@@ -19,26 +19,41 @@ class Externship {
       Outputs: None
       Appends students object to matched list
     */
+    studentObject.assignedExternship = this
     this.matched.push(studentObject);
   }
   getHighestUnmatched() {
     /*
       Inputs: None
-      Outputs: The highest ranked studentObject who is not ranked (not in matched)
+      Outputs: The highest ranked studentObject who is not ranked.
     */
     var i;
     for (i in this.applicants) {
-      var unmatched = true;
-      var j;
-      for (j in this.matched) {
-        if (this.applicants[i] === this.matched[j]) {
-          unmatched = false;
-          break;
-        }
-      }
-      if (unmatched) {
-        return this.applicants[i];
+      var applicant = this.applicants[i];
+      if (applicant.assignedExternship === null) {
+        return applicant;
       }
     }
+  }
+}
+
+class Student {
+  constructor(
+    email,
+    firstName=null,
+    lastName=null,
+    applications = [],
+  ) {
+    this.email = email
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.applications = applications;
+    this.assignedExternship = null;
+  }
+  addApplication(externshipObject){
+    /*
+      Simply appends an externship object to applications list
+    */
+    this.applications.push(externshipObject);
   }
 }
