c74535c60f0c1ecebdfa57334f952a3e0b44cb74
Jonathan Cai <caimjonathan@gmail.com>
2020-07-13 19:43:29 -0700

    match column selection

--- src/pages/Settings.js
+++ src/pages/Settings.js
@@ -2,38 +2,90 @@ import React from 'react';
 
 import { useRecoilState } from 'recoil';
 import { applicationState, leftDataState, rightDataState } from '../store/atoms';
+import { Select, Collapse } from 'antd';
 
-import { Card } from 'antd';
-
+const { Option } = Select;
+const { Panel } = Collapse;
 
 export default function Settings() {
   const [appState, setAppState] = useRecoilState(applicationState);
   const [leftData, setLeftData] = useRecoilState(leftDataState);
   const [rightData, setRightData] = useRecoilState(rightDataState);
 
+  function changeLeftMatchColumn(value) {
+    let column = leftData.columns[value];
+    setLeftData(oldLeftDataState => {
+      return {
+        ...oldLeftDataState,
+        matchColumn: column
+      }
+    })
+  }
+
+  function changeRightMatchColumn(value) {
+    let column = rightData.columns[value];
+    setRightData(oldRightDataState => {
+      return {
+        ...oldRightDataState,
+        matchColumn: column
+      }
+    })
+  }
+
   return (
     <div>
       <div className="Main">
         <div className="SettingsPanel">
 
-          <div>
-            <Card>
-              <h6>Left Columns:</h6>
+          <div style={{ width: "30%", paddingTop: 20 }}>
+            Left "match" column: &nbsp;
+            <Select value={leftData.matchColumn ? leftData.matchColumn.title : null} style={{ width: 250 }} onChange={changeLeftMatchColumn}>
               {leftData.columns.map((column, i) => {
                 return (
-                  <p key={i}>{column.title}</p>
+                  <Option key={i}>{column.title}</Option>
                 )
               })}
-            </Card>
+            </Select>
+
+            <br />
+            <br />
+
+            <Collapse accordion>
+              <Panel header="All left columns" key="1">
+                <h6>Left Columns:</h6>
+                {leftData.columns.map((column, i) => {
+                  return (
+                    <p key={i}>{column.fullTitle}</p>
+                  )
+                })}
+              </Panel>
+            </Collapse>
           </div>
-          <Card>
-            <h6>Right Columns:</h6>
-            {rightData.columns.map((column, i) => {
-              return (
-                <p key={i}>{column.title}</p>
-              )
-            })}
-          </Card>
+
+          <div style={{ width: "30%", paddingTop: 20 }}>
+            Right "match" column: &nbsp;
+            <Select value={rightData.matchColumn ? rightData.matchColumn.title : null} style={{ width: 250 }} onChange={changeRightMatchColumn}>
+              {rightData.columns.map((column, i) => {
+                return (
+                  <Option key={i}>{column.title}</Option>
+                )
+              })}
+            </Select>
+
+            <br />
+            <br />
+
+            <Collapse accordion>
+              <Panel header="All right columns" key="1">
+                <h6>Right Columns:</h6>
+                {rightData.columns.map((column, i) => {
+                  return (
+                    <p key={i}>{column.fullTitle}</p>
+                  )
+                })}
+              </Panel>
+            </Collapse>
+          </div>
         </div>
       </div>
     </div>
