0a8322c201ed142da3214afef8e55aef32eba652
Jonathan Cai <caimjonathan@gmail.com>
2020-07-15 19:29:09 -0700

    card around column settings panel

--- src/components/settings/ColumnSettingsPanel.js
+++ src/components/settings/ColumnSettingsPanel.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import { Select, Collapse, Checkbox } from 'antd';
+import { Select, Collapse, Checkbox, Card } from 'antd';
 
 const { Option } = Select;
 const { Panel } = Collapse;
 
 
-export default function ColumnSettingsPanel({data, setData, title}) {
-    
+export default function ColumnSettingsPanel({ data, setData, title }) {
+
     function changeColumn(value, columnType, data, setDataStateFunction) {
         let column = data.columns[value];
         setDataStateFunction(oldLeftDataState => {
@@ -35,48 +35,50 @@ export default function ColumnSettingsPanel({data, setData, title}) {
 
     return (
         <div style={{ width: "30%", paddingTop: 20 }}>
-            <div>
-                Match column: &nbsp;
+            <Card>
+                <div>
+                    Match column: &nbsp;
                 <Select value={data.matchColumn ? data.matchColumn.title : null} style={{ width: 250 }}
-                    onChange={(value) => changeColumn(value, 'matchColumn', data, setData)}>
-                    {data.columns.map((column, i) => {
-                        return (
-                            <Option key={i}>{column.title}</Option>
-                        )
-                    })}
-                </Select>
-            </div>
+                        onChange={(value) => changeColumn(value, 'matchColumn', data, setData)}>
+                        {data.columns.map((column, i) => {
+                            return (
+                                <Option key={i}>{column.title}</Option>
+                            )
+                        })}
+                    </Select>
+                </div>
 
-            <br />
+                <br />
 
-            <div>
-                Name column: &nbsp;
+                <div>
+                    Name column: &nbsp;
                 <Select value={data.nameColumn ? data.nameColumn.title : null} style={{ width: 250 }}
-                    onChange={(value) => changeColumn(value, 'nameColumn', data, setData)}>
-                    {data.columns.map((column, i) => {
-                        return (
-                            <Option key={i}>{column.title}</Option>
-                        )
-                    })}
-                </Select>
-            </div>
+                        onChange={(value) => changeColumn(value, 'nameColumn', data, setData)}>
+                        {data.columns.map((column, i) => {
+                            return (
+                                <Option key={i}>{column.title}</Option>
+                            )
+                        })}
+                    </Select>
+                </div>
 
-            <br />
+                <br />
 
-            <Collapse accordion>
-                <Panel header={title} key="1">
-                    <h6>Left Columns:</h6>
-                    {data.columns.map((column, i) => {
-                        return (
-                            <div key={i} style={{ display: "flex", flexDirection: "row" }}>
-                                <p style={{ width: 300 }}>{column.fullTitle}</p> &nbsp;
-                                <Checkbox checked={column.fixed === "left"} onChange={() => setColumnFixed(column, setData, "left")}>FL</Checkbox>
-                                <Checkbox checked={column.fixed === "right"} onChange={() => setColumnFixed(column, setData, "right")}>FR</Checkbox>
-                            </div>
-                        )
-                    })}
-                </Panel>
-            </Collapse>
+                <Collapse accordion>
+                    <Panel header={title} key="1">
+                        <h6>Left Columns:</h6>
+                        {data.columns.map((column, i) => {
+                            return (
+                                <div key={i} style={{ display: "flex", flexDirection: "row" }}>
+                                    <p style={{ width: 300 }}>{column.fullTitle}</p> &nbsp;
+                                    <Checkbox checked={column.fixed === "left"} onChange={() => setColumnFixed(column, setData, "left")}>FL</Checkbox>
+                                    <Checkbox checked={column.fixed === "right"} onChange={() => setColumnFixed(column, setData, "right")}>FR</Checkbox>
+                                </div>
+                            )
+                        })}
+                    </Panel>
+                </Collapse>
+            </Card>
         </div>
     )
 }
