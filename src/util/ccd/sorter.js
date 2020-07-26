9ed7130b113d089b98ce94fb99842388579e319f
sanjanaas689 <60405160+sanjanaas689@users.noreply.github.com>
2020-07-26 15:52:50 -0500

    Update sorter.js

--- src/util/ccd/sorter.js
+++ src/util/ccd/sorter.js
@@ -7,24 +7,5 @@ export function sortExternships(externships) {
         Output: sorted list of Externship objects by ascending order of "hotness"
         Hotness: define hotness to be the demand of the externship, difference between # of applicants and # of slots
     */
-
-    var priority_array = []
-    var sorted_externships = [];
-    var priority_list = {};
-
-    // mapping Externship --> hotness
-    for (var i = 0; i < externships.length; i++) {
-        priority_list[externships[i]] = externships[i].getPriority();
-    }
-
-    // sort externships by ascending hotness value
-    
-
-    // converts mapping to array i.e [Externship: priority]
-    priority_array = Object.entries(priority_list);
-    sorted_externships = priority_array.sort((a,b) => a[1]-b[1]);
-    
-    // returns the keys, Externship objects, in ascending order of hotness
-    sorted_externships = Object.keys(sorted_externships)
-    return sorted_externships;
+    return externships.sort((a,b) => a.getPriority()-b.getPriority())
 }
