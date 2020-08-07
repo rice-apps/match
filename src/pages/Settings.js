277d8d729767b0d6a6a1c0720728872b7d4e46f8
Ryan Knightly <reknightly@gmail.com>
2020-08-07 17:26:09 -0500

    Add sheet ID to settings

--- src/pages/Settings.js
+++ src/pages/Settings.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import { FormattedCard } from '../components/formatted-card/FormattedCard';
+import { Card } from 'antd';
+
 
 import { useRecoilState, useRecoilValue } from 'recoil';
 import { applicationState, leftDataState, rightDataState } from '../store/atoms';
 
 import ColumnSettingsPanel from '../components/settings/ColumnSettingsPanel';
+import Loader from '../components/loader/Loader';
 
 export default function Settings() {
   const { user } = useRecoilValue(applicationState);
@@ -20,6 +23,19 @@ export default function Settings() {
           </div>
           <ColumnSettingsPanel data={leftData} setData={setLeftData} title={"All left column settings"} />
           <ColumnSettingsPanel data={rightData} setData={setRightData} title={"All right column settings"} />
+        </div>
+        <div style={{ width: 600, paddingTop: 20 }}>
+          <Card title="Google Sheets">
+            {user ?
+            <div>
+              Left Spreadsheet ID
+              <Loader style={{"text-align": "left", "padding-top": "10px", "padding-bottom": "20px"}} onUpload={setLeftData} allowManualSort={true}/>
+              Right Spreadsheet ID
+              <Loader style={{"text-align": "left"}} onUpload={setRightData} allowManualSort={true}/>
+            </div> :
+            <div>Sign in to Google to use sheets.</div>
+            }
+          </Card>
         </div>
       </div>
     </div >
