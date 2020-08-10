eed5cc05616f71779a1ee7d4e76cc0b3860b328e
Ryan Knightly <reknightly@gmail.com>
2020-08-10 17:23:14 -0500

    Create SheetsSettingsPanel component

--- src/pages/Settings.js
+++ src/pages/Settings.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import { FormattedCard } from '../components/formatted-card/FormattedCard';
-import { Card } from 'antd';
-
 
 import { useRecoilState, useRecoilValue } from 'recoil';
 import { applicationState, leftDataState, rightDataState } from '../store/atoms';
 
 import ColumnSettingsPanel from '../components/settings/ColumnSettingsPanel';
-import Loader from '../components/loader/Loader';
+import SheetsSettingsPanel from '../components/settings/SheetsSettingsPanel';
 
 export default function Settings() {
   const { user } = useRecoilValue(applicationState);
@@ -24,19 +22,7 @@ export default function Settings() {
           <ColumnSettingsPanel data={leftData} setData={setLeftData} title={"All left column settings"} />
           <ColumnSettingsPanel data={rightData} setData={setRightData} title={"All right column settings"} />
         </div>
-        <div style={{ width: 600, paddingTop: 20 }}>
-          <Card title="Google Sheets">
-            {user ?
-            <div>
-              Left Spreadsheet ID
-              <Loader style={{"text-align": "left", "padding-top": "10px", "padding-bottom": "20px"}} onUpload={setLeftData} allowManualSort={true}/>
-              Right Spreadsheet ID
-              <Loader style={{"text-align": "left"}} onUpload={setRightData} allowManualSort={true}/>
-            </div> :
-            <div>Sign in to Google to use sheets.</div>
-            }
-          </Card>
-        </div>
+        <SheetsSettingsPanel setLeftData={setLeftData} setRightData = {setRightData}/>
       </div>
     </div >
   );
