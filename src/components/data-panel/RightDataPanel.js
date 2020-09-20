e8642e5b329e8f65b476e15a9a8c24815b9f8682
Adam Zawierucha <zawiedev@gmail.com>
2020-09-20 08:49:22 -0700

    fixed default match sorting

--- src/components/data-panel/RightDataPanel.js
+++ src/components/data-panel/RightDataPanel.js
@@ -30,7 +30,7 @@ export default function RightDataPanel(props) {
   // Here's where the sorting/filtering happens!!
   // Note selectedLeftRows[0]. Should only ever have one in the list anyways
   // as the left panel is "radio" select type.
-  const sortedData = applyRules(rules, data, selectedLeftRows[0], leftNameColumn, rightMatchColumn);
+  const sortedData = applyRules(rules, data, selectedLeftRows[0], leftEmailColumn, rightMatchColumn);
 
 
   // Takes in the left and right rows and determines if they're matched together
