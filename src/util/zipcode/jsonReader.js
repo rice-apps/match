e3b0923ac63a368b307a4a47e8af29c83dfa93f9
Adam Zawierucha <zawiedev@gmail.com>
2020-09-27 07:57:15 -0700

    docstring

--- src/util/zipcode/jsonReader.js
+++ src/util/zipcode/jsonReader.js
@@ -1,3 +1,10 @@
 function zipToCoordinates(zipcode) {
-    
+    /**
+     * Takes in a Zipcode (int) and returns a coordinate {Longitude:(float), Lattitude(float)}
+     * by reading from a JSON file
+     * 
+     * Input: (int) zipcode
+     * Output: {Longitude:(float),Lattitude:(float)}
+     */
+    return {Longitude:0, Lattitude:0};
 }
