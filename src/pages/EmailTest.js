33cf69a3a1157b221a0d46ec41fbd91a22f23cf0
Adam Zawierucha <zawiedev@gmail.com>
2020-11-08 09:18:43 -0800

    getter current sender

--- src/pages/EmailTest.js
+++ src/pages/EmailTest.js
@@ -9,7 +9,7 @@ import { applicationState } from '../store/atoms';
 export default function EmailTest() {
     const { user } = useRecoilValue(applicationState);
     if(user){
-        return <Button onClick = {() => composeAndSendEmail("adz2@rice.edu","bdu1@rice.edu","Wassup Homie","Wow bottom text!")}>SEND EMAIL BOI</Button>
+        return <Button onClick = {() => composeAndSendEmail("bdu1@rice.edu","Wassup Homie","Wow bottom text!")}>SEND EMAIL BOI</Button>
     } else {
         return <p>Please login idiot.</p>
     }
